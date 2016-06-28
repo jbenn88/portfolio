a355b7d5a37bdc896d7242f73850952df09fb99d
James <James>
2016-06-27 20:54:57 -0400

    Adds mouseenter/mouseleave effect on nav buttons and general button // changes jquery source from local to Google CDN

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-  $('.nav li').hover(function(){
-    $(this).addClass('active');
-  },
-  function() {
-    $(this).removeClass('active');
-  }
-);
+  $('.nav li').fadeTo('slow', 0.3);
+    $(".nav li").mouseenter(function(){
+        $(this).fadeTo("slow", 1);
+        $(this).css("color: red");
+    });
+    $('.nav li').mouseleave(function (){
+      $(this).fadeTo('slow', 0.3);
+    });
+
+    $('.button').mouseenter(function () {
+      $(this).addClass('active');
+    });
+    $('.button').mouseleave(function (){
+      $(this).removeClass('active');
+    });
+
 });
